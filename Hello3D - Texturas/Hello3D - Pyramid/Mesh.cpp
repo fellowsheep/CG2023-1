a2e28d32497ff8ec633973e2d5e32cdf2876d9f5
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-05-17 20:40:12 -0300

    Adicionado projeto Hello 3D - Texturas - ainda sem grupos

---
+++ Hello3D - Texturas/Hello3D - Pyramid/Mesh.cpp
@@ -0,0 +1,30 @@
+#include "Mesh.h"
+
+void Mesh::initialize(GLuint VAO, int nVertices, Shader* shader, GLuint texID, glm::vec3 position, glm::vec3 scale, float angle, glm::vec3 axis)
+{
+	this->VAO = VAO;
+	this->nVertices = nVertices;
+	this->shader = shader;
+	this->position = position;
+	this->scale = scale;
+	this->angle = angle;
+	this->axis = axis;
+	this->texID = texID;
+}
+
+void Mesh::update()
+{
+	glm::mat4 model = glm::mat4(1); //matriz identidade
+	model = glm::translate(model, position);
+	model = glm::rotate(model, glm::radians(angle), axis);
+	model = glm::scale(model, scale);
+	shader->setMat4("model", glm::value_ptr(model));
+}
+
+void Mesh::draw()
+{
+	glBindTexture(GL_TEXTURE_2D, texID);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, nVertices);
+	glBindVertexArray(0);
+}
